dbd539b349c3a8a3c60edc1580faa380bf6986d4
quiramapino347 <quiramapino347@gmail.com>
2026-05-29 13:33:36 -0500

    organizando py y main

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hola Docker y kubernettes")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hola Docker y Kubernetes"
+
+app.run(host="0.0.0.0", port=5000)
