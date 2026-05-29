443b47544af28b8cfcb3f785ccd5309e9ecb5517
quiramapino347 <quiramapino347@gmail.com>
2026-05-29 16:39:58 -0500

    cambiando app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola Docker y Kubernetes"
+    return "Esto es un proyecto DevOps con Docker y Kubernettes"
 
 app.run(host="0.0.0.0", port=5000)
